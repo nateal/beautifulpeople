bbaa555738f21f78a732a5b7b72b79f9a7e12a35
nateal <60979241+nateal@users.noreply.github.com>
2020-02-13 19:22:55 +0800

    Add files via upload
    Commit 1: GET, POST, DELETE, PUT endpoints created; no database connected yet

--- test.js
+++ test.js
@@ -1,14 +1,69 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+//Array standin for database
+const inv = [
+	{id: 1, items: 'test1'},
+	{id: 2, items: 'test2'},
+	{id: 3, items: 'test3'},
+];
+
 
 app.get('/', (request, respond) => {
 	respond.send('Hello World!!!');
 });
 
-app.get('/api/courses', (request, respond) => {
-	respond.send ([1,2,3]);
+app.get('/api/inv', (request, respond) => {
+	respond.send (inv);
 });
 
+//GET
+app.get('/api/inv/:any', (request, respond) => {
+	const inventory = inv.find(c => c.id === parseInt(request.params.any));
+	if(!inventory){
+		respond.status(404).send('unable to find id');
+	}
+	else
+		respond.send(inventory);
+});
+
+//POST
+app.post('/api/inv', (request, respond) => {
+	const inventory = {
+		id: inv.length + 1,
+		items: request.body.items
+	};
+	inv.push(inventory);
+	respond.send(inventory);
+});
+
+//PUT
+app.put('/api/inv/:any', (request, respond) => {
+	const inventory = inv.find(c => c.id === parseInt(request.params.any));
+	if(!inventory){
+		respond.status(404).send('unable to find id');
+	}
+
+	else{
+		inventory.items = request.body.items;
+		respond.send(inventory);
+
+	}
+});
+
+//DELETE
+app.delete('/api/inv/:any', (request, respond) => {
+	const inventory = inv.find(c => c.id === parseInt(request.params.any));
+	if(!inventory){
+		respond.status(404).send('unable to find id');
+	}
+
+	const index = inv.indexOf(inventory);
+	inv.splice(index, 1);
+	respond.send(inventory);
+
+});
 
 // PORT
 const port = process.env.PORT || 3000;
